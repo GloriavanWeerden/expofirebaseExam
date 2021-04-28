564a99c05cddaf962a3f75d83a12148b36dfa416
Gloria van Weerden <gloria.vanweerden@gmail.com>
2021-04-27 20:52:22 -0400

    adding config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,14 @@ import '@firebase/auth';
 import '@firebase/database';
 
 const firebaseConfig = {
-    apiKey: "Copy Your config from the firebase console"
+    apiKey: "AIzaSyCcFbHDvSrhfiyhIO_jkjFHVVuyIU-O_mg",
+    authDomain: "finalexam-82022.firebaseapp.com",
+    databaseURL:"https://finalexam-82022-default-rtdb.firebaseio.com/",
+    projectId: "finalexam-82022",
+    storageBucket: "finalexam-82022.appspot.com",
+    messagingSenderId: "1070209354294",
+    appId: "1:1070209354294:web:4889003a592a87c20fe697",
+    measurementId: "G-Z9CRLGVV5E"
 };
 
 if (!firebase.apps.length) {
